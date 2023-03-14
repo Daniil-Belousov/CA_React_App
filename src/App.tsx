6086512e47ad931e08fdd5f74c4c8caf212b517f
Daniil Belousov <d.belousov.exceedteam@gmail.com>
2023-03-14 21:03:11 +0300

    fix fifth task

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,17 @@ function App() {
         <p>
           Hello CA!
         </p>
-        {!isOpen ? <button className='menuBtn' onClick={() => setOpen(!isOpen)}>Menu</button> 
-        : <button style={{color: 'red'}} className='menuBtn' onClick={() => setOpen(!isOpen)}>Close</button>}
-        <div className='list' style={!isOpen ? {visibility: 'hidden'} : {visibility: 'visible'}}>
-          <div className='item'>Личный кабинет</div>
-          <div className='item'>Настройки</div>
-          <div className='item'>Адрес</div>
-          <div className='item'>Контакты</div>
-          <div className='item'>О нас</div>
-        </div>
+        {!isOpen && <button className='menuBtn' onClick={() => setOpen(!isOpen)}>Menu</button>}
+        {isOpen && <button style={{color: 'red'}} className='menuBtn' onClick={() => setOpen(!isOpen)}>Close</button>}
+        {isOpen &&
+          <div className='list'>
+            <div className='item'>Личный кабинет</div>
+            <div className='item'>Настройки</div>
+            <div className='item'>Адрес</div>
+            <div className='item'>Контакты</div>
+            <div className='item'>О нас</div>
+          </div>
+        } 
       </header>
     </div>
   );
