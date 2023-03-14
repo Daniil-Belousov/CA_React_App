603100c1b445c731f75d95c00471457b9f4f8b1f
Daniil Belousov <d.belousov.exceedteam@gmail.com>
2023-03-14 13:49:05 +0300

    eighth task

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello CA!
-        </p>
-      </header>
-    </div>
+import { useMemo, useState, useCallback } from "react";
+
+const App = (): JSX.Element => {
+  const items = ['первый', 'второй', 'третий', 'четвертый', 'пятый'];
+  const [list, setList] = useState([...items]);
+
+  const handleRemoveItem = useCallback(
+    (index: number) => {
+      const newList = [...list];
+      newList.splice(index, 1);
+      setList(newList);
+    },
+    [list]
   );
-}
+
+  const renderedList = useMemo(() => list.map((item, index) => (
+    <li key={index}>
+      {item} <button onClick={() => handleRemoveItem(index)}>x</button>
+    </li>
+  )), [list, handleRemoveItem]);
+
+  return (
+    <ul>
+      {renderedList}
+    </ul>
+  );
+};
 
 export default App;
