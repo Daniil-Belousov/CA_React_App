847a48e1f8a9df6426a61d403559477871cb5fa4
Daniil Belousov <d.belousov.exceedteam@gmail.com>
2023-03-14 20:53:32 +0300

    fix third task

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Modal from './Modal';
 
 function App() {
   const [isOpenModal, setIsOpenModal] = useState(false);
 
+  useEffect(() => {
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.key === "Escape") {
+        setIsOpenModal(false);
+      }
+    }
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+    
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header" onClick={() => isOpenModal && setIsOpenModal(false)}>
         <p>
           Hello CA!
         </p>
